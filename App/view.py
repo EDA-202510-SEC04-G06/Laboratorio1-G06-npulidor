ff32f5353aa1a0553e020cef01f2a49c81ddc93a
NataliaPR22 <n.pulidor@uniandes.edu.co>
2025-01-29 10:35:09 -0500

    modificaciones por debug lab 1

--- App/view.py
+++ App/view.py
@@ -60,6 +60,7 @@ def print_menu():
     # TODO: Mods Lab 1, agregar la opcion 3.
     # Agregue la opción 3 para cargar los tags de los libros.
     # Pueede guiarse de las opciones 1 y 2.
+    print('3- Cargar Tags de los libros')
     print("0- Salir")
 
 
@@ -97,8 +98,7 @@ def load_books_tags(app):
     :param app: Aplicación de la lógica
     :type app: logic
     """
-    booksTags = logic.load_books_tags(app,
-                                      "GoodReads/book_tags-small.csv")
+    booksTags = logic.load_books_tags(app, "GoodReads/book_tags-small.csv")
     return booksTags
 
 
@@ -135,6 +135,10 @@ def main():
         # Puede guiarse de las opciones 1 y 2.
         # Imprima el total de tags de los libros cargados.
         elif int(inputs[0]) == 3:
+            print('Cargando información de los libros....')
+            booksTags = load_books_tags(app)
+            print('Total de tags de los libros cargados: ' + str(booksTags) + '\n')
+        elif int(inputs[0]) == 3: 
             pass
 
         elif int(inputs[0]) == 0:
